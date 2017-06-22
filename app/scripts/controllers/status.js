af9a2a861ccbd62ffb0574527cd2430db4f5f511
Andrew S. Brown, Ph.D <abrown3@nmdp.org>
2017-06-22 14:28:19 -0500

    Able to download

--- app/scripts/controllers/status.js
+++ app/scripts/controllers/status.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  function status($scope, statusService) {
+  function status($scope, statusService, downloadService) {
     /*jshint validthis: true */
     var statusCtrl = this;
 
@@ -9,12 +9,36 @@
     statusCtrl.statuses = [];
 
     statusCtrl.refresh = function () {
-        statusService.getStatus(5).then(function (result) {
-            statusCtrl.statuses = result;
-        });
+      statusService.getStatus(5).then(function (result) {
+          statusCtrl.statuses = result;
+      });
+    };
+
+    statusCtrl.downloadHmlXml = function (status) {
+      downloadService.downloadHml(status.hmlId, true).then(function () {
+
+      });
+    };
+
+    statusCtrl.downloadHmlJson = function (status) {
+      downloadService.downloadHml(status.hmlId).then(function () {
+
+      });
+    };
+
+    statusCtrl.downloadFhirXml = function (status) {
+      downloadService.downloadFhir(status.hmlId, true).then(function () {
+
+      });
+    };
+
+    statusCtrl.downloadFhirJson = function (status) {
+      downloadService.downloadFhir(status.hmlId).then(function () {
+
+      });
     };
   }
 
   angular.module('webHmlFhirConversionDashboardApp.controllers').controller('status', status);
-  status.$inject = ['$scope', 'statusService'];
+  status.$inject = ['$scope', 'statusService', 'downloadService'];
 }());
