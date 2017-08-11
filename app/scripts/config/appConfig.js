83e594b396d656141dd09ccd4a5d65506e4fed28
Andrew S. Brown, Ph.D <abrown3@nmdp.org>
2017-08-11 10:36:44 -0500

    Fix port conflicts with docker

--- app/scripts/config/appConfig.js
+++ app/scripts/config/appConfig.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var appConfig = {
-      'conversion_server_url': 'http://localhost:8091/v1/'
+      'conversion_server_url': 'http://localhost:8099/v1/'
   };
 
   angular.module('webHmlFhirConversionDashboardApp.config').constant('appConfig', appConfig);
